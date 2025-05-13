e0a16c35de44f3a3fff89e3815acd58b15d7b2d1
ecernice <eliska.cerna@email.cz>
2025-05-13 11:47:18 +0200

    Basic structure of project was created. Main class created. Dependency Spring Boot Starter Web added in pom.xml

---
+++ src/main/java/cz/itnetwork/insurancerecords/InsuranceRecordsApplication.java
@@ -0,0 +1,13 @@
+package cz.itnetwork.insurancerecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceRecordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceRecordsApplication.class, args);
+    }
+
+}
