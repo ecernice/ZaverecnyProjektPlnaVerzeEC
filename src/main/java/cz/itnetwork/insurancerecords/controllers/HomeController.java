2132a7e693306d849c12b19d4f7b745c84a7890c
ecernice <eliska.cerna@email.cz>
2025-05-14 11:38:15 +0200

    More detailed structure of web pages. HomeController.

---
+++ src/main/java/cz/itnetwork/insurancerecords/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package cz.itnetwork.insurancerecords.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String renderIndex (){
+        return "pages/home/index";
+    }
+
+}
