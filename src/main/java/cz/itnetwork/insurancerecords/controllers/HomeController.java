6c2645c117a0173af4fbe7fd1f7d61c8b0f3dea4
ecernice <eliska.cerna@email.cz>
2025-05-14 16:01:15 +0200

    Basic code of html pages using thymeleaf fragments. Controllers.

--- src/main/java/cz/itnetwork/insurancerecords/controllers/HomeController.java
+++ src/main/java/cz/itnetwork/insurancerecords/controllers/HomeController.java
@@ -7,8 +7,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String renderIndex (){
+    public String renderIndex () {
         return "pages/home/index";
+    }
+
+    @GetMapping("/about")
+    public String renderAbout() {
+        return "pages/home/about";
+    }
+
+    @GetMapping("/incidents")
+    public String renderIncidents() {
+        return "pages/home/incidents";
+    }
+
+    @GetMapping("/insurances")
+    public String renderInsurances() {
+        return "pages/home/insurances";
+    }
+
+    @GetMapping("/insureds")
+    public String renderInsureds() {
+        return "pages/home/insureds";
     }
 
 }
