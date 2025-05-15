644fe914e485654505ebb53f473889dee605b527
ecernice <eliska.cerna@email.cz>
2025-05-15 17:52:47 +0200

    Functional pages and controllers for account: registration and login.

--- src/main/java/cz/itnetwork/insurancerecords/controllers/AccountController.java
+++ src/main/java/cz/itnetwork/insurancerecords/controllers/AccountController.java
@@ -1,11 +1,23 @@
 package cz.itnetwork.insurancerecords.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping ("/account")
 public class AccountController {
 
+    @GetMapping("/login")
+    public String renderLogin() {
+
+        return "pages/account/login";
+    }
+
+    @GetMapping ("/registration")
+    public String renderRegistration() {
+
+        return "pages/account/registration";
+    }
 
 }
