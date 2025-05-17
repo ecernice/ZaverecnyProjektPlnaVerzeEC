adfe97d87e13be8f7b79c28c590f7640efec0b3f
ecernice <eliska.cerna@email.cz>
2025-05-17 17:37:40 +0200

    insured/created.html: thymeleaf atributes in the form mapped to DTO atributes. Atribut th:errors for validations. Controller: tunning console messages added.

--- src/main/java/cz/itnetwork/insurancerecords/controllers/InsuredController.java
+++ src/main/java/cz/itnetwork/insurancerecords/controllers/InsuredController.java
@@ -1,9 +1,12 @@
 package cz.itnetwork.insurancerecords.controllers;
 
 import cz.itnetwork.insurancerecords.models.dto.InsuredDTO;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,7 +21,7 @@ public class InsuredController {
 
     @GetMapping ("/create")
     public String renderCreateForm(@ModelAttribute InsuredDTO insured) {
-
+        System.out.println("Zobrazuji formulář pro nového pojištěnce");
         return "pages/database/insureds/create";
     }
 
@@ -32,6 +35,21 @@ public class InsuredController {
     public String renderEdit() {
 
         return "pages/database/insureds/edit";
+    }
+
+    @PostMapping("create")
+    public String createInsured(
+            @Valid @ModelAttribute InsuredDTO insured,
+            BindingResult result
+    ) {
+        if (result.hasErrors()){
+            System.out.println("Formulář obsahuje chyby:" + result.getAllErrors());
+            return renderCreateForm(insured);}
+
+        // Zde budeme později pracovat s databází
+        System.out.println(insured.getSurname() + " – " + insured.getPhoneNumber());
+
+        return "redirect:/articles";
     }
 
 }
