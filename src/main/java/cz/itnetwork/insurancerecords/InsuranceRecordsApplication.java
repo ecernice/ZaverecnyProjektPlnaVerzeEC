cca95bb0f3ae30d1cba0f690e8c4d471337b24ff
ecernice <eliska.cerna@email.cz>
2025-05-18 11:32:31 +0200

    New dependencies: starter data jpa and mariadb java client. Jpa enabled in main class. Database connection setting in application.properties.

--- src/main/java/cz/itnetwork/insurancerecords/InsuranceRecordsApplication.java
+++ src/main/java/cz/itnetwork/insurancerecords/InsuranceRecordsApplication.java
@@ -2,8 +2,10 @@ package cz.itnetwork.insurancerecords;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class InsuranceRecordsApplication {
 
     public static void main(String[] args) {
