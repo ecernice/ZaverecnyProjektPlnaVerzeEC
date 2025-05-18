5eb98c11bfe6be4acf8774ef7249ae860f581dc4
ecernice <eliska.cerna@email.cz>
2025-05-18 10:56:13 +0200

    Incident: controller, DTO, creation html template, validations.

--- src/main/java/cz/itnetwork/insurancerecords/controllers/IncidentController.java
+++ src/main/java/cz/itnetwork/insurancerecords/controllers/IncidentController.java
@@ -1,7 +1,13 @@
 package cz.itnetwork.insurancerecords.controllers;
 
+import cz.itnetwork.insurancerecords.models.dto.IncidentDTO;
+import cz.itnetwork.insurancerecords.models.dto.InsuranceDTO;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,8 +21,8 @@ public class IncidentController {
     }
 
     @GetMapping ("/create")
-    public String renderCreate() {
-
+    public String renderCreateForm(@ModelAttribute IncidentDTO incident) {
+        System.out.println("Zobrazuji formulář pro novou pojistnou událost");
         return "pages/database/incidents/create";
     }
 
@@ -30,6 +36,21 @@ public class IncidentController {
     public String renderEdit() {
 
         return "pages/database/incidents/edit";
+    }
+
+    @PostMapping("create")
+    public String createIncident(
+            @Valid @ModelAttribute IncidentDTO incident,
+            BindingResult result
+    ) {
+        if (result.hasErrors()){
+            System.out.println("Formulář obsahuje chyby:" + result.getAllErrors());
+            return renderCreateForm(incident);}
+
+        // Zde budeme později pracovat s databází
+        System.out.println(incident.getTitle() + " – " + incident.getIncidentDate());
+
+        return "redirect:/incidents";
     }
 
 }
---
+++ src/main/java/cz/itnetwork/insurancerecords/models/dto/IncidentDTO.java
@@ -0,0 +1,54 @@
+package cz.itnetwork.insurancerecords.models.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public class IncidentDTO {
+
+    @NotBlank (message = "Vyplňte název události")
+    private String title;
+
+    @NotBlank (message = "Vyplňte stručný popis události")
+    private String description;
+
+    @NotNull (message = "Vyplňte datum události")
+    private LocalDate incidentDate;
+
+    @NotNull (message = "Uvěďte částku pojistného plnění")
+    private int insuranceAmount;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getIncidentDate() {
+        return incidentDate;
+    }
+
+    public void setIncidentDate(LocalDate incidentDate) {
+        this.incidentDate = incidentDate;
+    }
+
+    public int getInsuranceAmount() {
+        return insuranceAmount;
+    }
+
+    public void setInsuranceAmount(int insuranceAmount) {
+        this.insuranceAmount = insuranceAmount;
+    }
+}
